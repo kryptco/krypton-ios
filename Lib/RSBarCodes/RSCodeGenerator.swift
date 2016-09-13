add92df289b02e7f1ce9abf9b457504a478c8dcb
Alex Grinman <grinman.alex@gmail.com>
2016-09-13 13:49:56 -0400

    checkpoint on modal view for scanning

--- Lib/RSBarCodes/RSCodeGenerator.swift
+++ Lib/RSBarCodes/RSCodeGenerator.swift
@@ -185,8 +185,8 @@ public class RSAbstractCodeGenerator : RSCodeGenerator {
                 
                 colorFilter.setDefaults()
                 colorFilter.setValue(filter.outputImage, forKey: "inputImage")
-                colorFilter.setValue(UIColor.white.coreImageColor, forKey: "inputColor0")
-                colorFilter.setValue(UIColor.app.coreImageColor, forKey: "inputColor1")
+                colorFilter.setValue(UIColor.clear.coreImageColor, forKey: "inputColor0")
+                colorFilter.setValue(UIColor.black.coreImageColor, forKey: "inputColor1")
                 ///
                 
                 let outputImage = colorFilter.outputImage
--- krSSH/PairController.swift
+++ krSSH/PairController.swift
@@ -16,17 +16,74 @@ class PairController: UIViewController, KRScanDelegate {
     @IBOutlet var scanViewController:KRScanController?
     @IBOutlet weak var scanRails:UIImageView!
 
+    @IBOutlet weak var blurView:UIView!
+    
+    @IBOutlet weak var popupView:UIView!
+    @IBOutlet weak var subjectLabel:UILabel!
+    @IBOutlet weak var messageLabel:UILabel!
+    
+    @IBOutlet weak var rejectButton:UIButton!
+    @IBOutlet weak var approveButton:UIButton!
+    
+    @IBOutlet weak var result:UIImageView!
+    
+    
+    enum ResultImage:String {
+        case check = "check"
+        case x = "x"
+        
+        var image:UIImage? {
+            return UIImage(named: self.rawValue)
+        }
+    }
+    
+    
+    enum Scanned {
+        case peer(Peer)
+        case pairing(Pairing)
+        
+        var subject:String {
+            switch self {
+            case .peer(let p):
+                return p.email.uppercased()
+            case .pairing(let p):
+                return p.name.uppercased()
+            }
+        }
+        
+        var message:String {
+            switch self {
+            case .peer(let p):
+                return "Do you want save this as \(p.email)'s public key?"
+            case .pairing(let p):
+                return "Do you want to pair with \"\(p.name)\"? This device will be able to request SSH logins using your private key."
+            }
+        }
+    }
+
+    var currentScanned:Scanned?
+
+
     required init?(coder aDecoder: NSCoder) {
         super.init(coder: aDecoder)
     }
  
     override func viewDidLoad() {
         super.viewDidLoad()
+        
+        popupView.isHidden = true
+        blurView.isHidden = true
+        result.isHidden = true
+
+        popupView.setBorder(color: UIColor.black.withAlphaComponent(0.2), cornerRadius: 40, borderWidth: 0.0)
     }
     
     override func viewWillAppear(_ animated: Bool) {
         super.viewWillAppear(animated)
         self.navigationController?.navigationBar.barTintColor = UIColor.app
+        
+        rejectButton.imageView?.contentMode = UIViewContentMode.scaleAspectFit
+        approveButton.imageView?.contentMode = UIViewContentMode.scaleAspectFit
         
         self.scanViewController?.canScan = true
     }
@@ -43,6 +100,72 @@ class PairController: UIViewController, KRScanDelegate {
         }
     }
     
+    //MARK: Animate Popover
+    
+    func showPopup(scanned:Scanned) {
+        subjectLabel.text = scanned.subject
+        messageLabel.text = scanned.message
+        
+        popupView.alpha = 0
+        blurView.alpha = 0
+
+        popupView.isHidden = false
+        blurView.isHidden = false
+        
+        UIView.animate(withDuration: 0.5) {
+            self.popupView.alpha = 1
+            self.blurView.alpha = 1
+        }
+   
+    }
+    
+    func hidePopup(success:Bool) {
+        
+        if success {
+            result.image = ResultImage.check.image
+        } else {
+            result.image = ResultImage.x.image
+        }
+        
+        result.alpha = 0
+        result.isHidden = false
+
+        UIView.animate(withDuration: 0.5, animations:
+            {
+                self.popupView.alpha = 0
+                self.result.alpha = 1
+                
+                
+        }) { (s) in
+            self.popupView.isHidden = true
+            
+            UIView.animate(withDuration: 1.0, animations: {
+                self.result.alpha = 0
+                self.blurView.alpha = 0
+
+                }, completion: { (_) in
+                    self.result.isHidden = true
+                    self.blurView.isHidden = true
+                    self.scanViewController?.canScan = true
+            })}
+    }
+        
+    //MARK: Accept Reject
+        
+    @IBAction func acceptTapped() {
+        if let scanned = currentScanned {
+            approve(scanned: scanned)
+        }
+        
+        hidePopup(success: true)
+        currentScanned = nil
+    }
+    
+    @IBAction func rejectTapped() {
+        hidePopup(success: false)
+        currentScanned = nil
+    }
+    
 
     //MARK: KRScanDelegate
     func onFound(data:String) -> Bool {
@@ -55,9 +178,28 @@ class PairController: UIViewController, KRScanDelegate {
         
         
         if let peer = try? Peer(json: json) {
-            PeerManager.shared.add(peer: peer)
+            let scanned = Scanned.peer(peer)
+            currentScanned = scanned
+            dispatchMain { self.showPopup(scanned: scanned) }
             return true
+            
         } else if let pairing = try? Pairing(json: json) {
+            let scanned = Scanned.pairing(pairing)
+            currentScanned = scanned
+            dispatchMain { self.showPopup(scanned: scanned) }
+            return true
+        }
+        
+    
+        return false
+    }
+    
+    
+    //MARK: Approve Scanned
+    
+    func approve(scanned:Scanned) {
+        switch scanned {
+        case .pairing(let pairing):
             do {
                 let session = try Session(pairing: pairing)
                 SessionManager.shared.add(session: session)
@@ -67,16 +209,14 @@ class PairController: UIViewController, KRScanDelegate {
             }
             catch let e {
                 log("error creating session: \(e)", .error)
-                return false
             }
             dispatchAfter(delay: 1.0, task: {
                 self.scanViewController?.canScan = true
             })
-            return true
+
+        case .peer(let peer):
+            PeerManager.shared.add(peer: peer)
         }
-        
-    
-        return false
     }
 
 }
