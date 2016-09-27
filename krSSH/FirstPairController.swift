80559e669443304f495ebe51e58aff549a8f114a
Alex Grinman <grinman.alex@gmail.com>
2016-09-27 11:36:07 -0400

    checkpoint on first pair

---
+++ krSSH/FirstPairController.swift
@@ -0,0 +1,14 @@
+//
+//  FirstPairController.swift
+//  krSSH
+//
+//  Created by Alex Grinman on 9/27/16.
+//  Copyright © 2016 KryptCo. All rights reserved.
+//
+
+import Foundation
+import UIKit
+
+class FirstPairController:UIViewController {
+    
+}
