e1a1439144a548c1ca5186be5f5de53514db6496
Alex Grinman <grinman.alex@gmail.com>
2017-03-16 20:59:16 -0400

    remove weird tab. period instead of asterisk

--- Notify/Policy+UI.swift
+++ Notify/Policy+UI.swift
@@ -12,13 +12,13 @@ import JSON
 
 extension Policy {
     class func requestUserAuthorization(session:Session, request:Request) {
-        NotificationService.shared?.alertTitle = "Request from \(session.pairing.displayName) *"
+        NotificationService.shared?.alertTitle = "Request from \(session.pairing.displayName)."
         NotificationService.shared?.approved = false
 
     }
     
     class func notifyUser(session:Session, request:Request) {
-        NotificationService.shared?.alertTitle = "Approved r    equest from \(session.pairing.displayName) *"
+        NotificationService.shared?.alertTitle = "Approved request from \(session.pairing.displayName)."
         NotificationService.shared?.approved = true
     }
 }
