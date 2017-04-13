7e6e501c444ef1ecef745c41128bde3dcadf7180
Kevin King <4kevinking@gmail.com>
2017-04-13 11:50:00 -0400

    migrate analytics_disabled setting to UserDefaults group

--- Kryptonite/AppDelegate.swift
+++ Kryptonite/AppDelegate.swift
@@ -21,6 +21,7 @@ class AppDelegate: UIResponder, UIApplicationDelegate {
     func application(_ application: UIApplication, willFinishLaunchingWithOptions launchOptions: [UIApplicationLaunchOptionsKey : Any]? = nil) -> Bool {
 
         Analytics.migrateOldIDIfExists()
+        Analytics.migrateAnalyticsDisabled()
                 
         Resources.makeAppearences()
         
