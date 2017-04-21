f71291abd0e8742acde5fd36a17ffb9ed93fbf51
Alex Grinman <grinman.alex@gmail.com>
2017-04-21 15:59:55 -0400

    update json framework, default pretty print off

--- Kryptonite/SignatureLog.swift
+++ Kryptonite/SignatureLog.swift
@@ -19,7 +19,7 @@ struct SignatureLog {
     init(session:String, hostAuth:HostAuth?, signature:String, displayName:String, date:Date = Date()) {
         
         var theHostAuth:String
-        if let host = hostAuth, let hostJson = try? host.jsonString(prettyPrinted: false) {
+        if let host = hostAuth, let hostJson = try? host.jsonString() {
             theHostAuth = hostJson
         } else {
             theHostAuth = "unknown"
