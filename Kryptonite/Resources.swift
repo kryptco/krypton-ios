cba9f18f9e11203cd008a99d37deb6f2650a14dd
Alex Grinman <grinman.alex@gmail.com>
2017-05-09 20:07:06 -0400

    ux tweaks

--- Kryptonite/Resources.swift
+++ Kryptonite/Resources.swift
@@ -22,12 +22,12 @@ struct Resources {
         UINavigationBar.appearance().tintColor = UIColor.app
         
         
-        if let font = UIFont(name: "AvenirNext-DemiBold", size: 17) {
+        
+        if let font = UIFont(name: "AvenirNext-Bold", size: 18) {
             UINavigationBar.appearance().titleTextAttributes = [
-                NSForegroundColorAttributeName: UIColor.black,
+                NSForegroundColorAttributeName: UIColor(hex: 0x555555),
                 NSFontAttributeName: font,
             ]
-    
         }
 
 
