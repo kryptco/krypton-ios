24787b4204b4338405371c7be407d5d3f15a289d
Alex Grinman <alex@krypt.co>
2017-08-30 13:56:27 -0400

    add conflict tests

--- KryptoTests/TeamDataManagerTests.swift
+++ KryptoTests/TeamDataManagerTests.swift
@@ -41,4 +41,51 @@ class TeamDataManagerTests: XCTestCase {
         }
     }
     
+    func testConflicts() {
+        let team = Team(info: Team.Info(name: "test team"))
+        let createPayload = try! HashChain.CreateChain(teamPublicKey: teamPublicKey, teamInfo: team.info).jsonString()
+        let createSignature = try! Data.random(size: 64)
+        let createBlock = HashChain.Block(payload: createPayload, signature: createSignature)
+        
+        let dm = TeamDataManager(teamID: id)
+        
+        do {
+            try dm.create(team: team, block: createBlock)
+            try dm.saveContext()
+            
+        } catch {
+            XCTFail("\(error)")
+        }
+        
+        let b1 = HashChain.Block(payload: "some 1", signature: try! Data.random(size: 64))
+        let b2 = HashChain.Block(payload: "some 2", signature: try! Data.random(size: 64))
+
+        do {
+            let dm1 = TeamDataManager(teamID: id)
+            try dm1.append(block: b1)
+            
+            let dm2 = TeamDataManager(teamID: id)
+            try dm2.append(block: b2)
+            
+            try dm1.saveContext()
+            try dm2.saveContext()
+
+            XCTFail("Error: should have found conflicts")
+        } catch {
+        }
+        
+        do {
+            let dm1 = TeamDataManager(teamID: id)
+            try dm1.append(block: b1)
+            try dm1.saveContext()
+            
+            let dm2 = TeamDataManager(teamID: id)
+            try dm2.append(block: b2)
+            try dm2.saveContext()
+        } catch {
+            XCTFail("\(error)")
+        }
+
+    }
+    
 }
