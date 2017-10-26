04f8ab017e57ceaf2ff0bc318bbe3e94bb0c5594
Alex Grinman <alex@krypt.co>
2017-10-26 14:02:02 -0400

    dispatch main on policy ui request/notify logic

--- Kryptonite/Policy+UI.swift
+++ Kryptonite/Policy+UI.swift
@@ -50,89 +50,91 @@ extension Policy {
     
     class func requestUserAuthorization(session:Session, request:Request) {
         
-        switch UIApplication.shared.applicationState {
-            
-        case .background: // Background: then present local notification
-            Notify.shared.present(request: request, for: session)
-            
-        case .inactive: // Inactive: wait and try again
-            dispatchAfter(delay: 1.0, task: {
-                Policy.requestUserAuthorization(session: session, request: request)
-            })
-
-        case .active:
-            // if we are already presenting, don't try to present until finished
-            guard Current.viewController?.presentedViewController is ApproveController == false
-            else {
-                return
-            }
-            
-            // current view controller hasn't loaded, but application active
-            if Current.viewController == nil {
+        dispatchMain {
+            switch UIApplication.shared.applicationState {
+                
+            case .background: // Background: then present local notification
+                Notify.shared.present(request: request, for: session)
+                
+            case .inactive: // Inactive: wait and try again
                 dispatchAfter(delay: 1.0, task: {
                     Policy.requestUserAuthorization(session: session, request: request)
                 })
-                return
+                
+            case .active:
+                // if we are already presenting, don't try to present until finished
+                guard Current.viewController?.presentedViewController is ApproveController == false
+                    else {
+                        return
+                }
+                
+                // current view controller hasn't loaded, but application active
+                if Current.viewController == nil {
+                    dispatchAfter(delay: 1.0, task: {
+                        Policy.requestUserAuthorization(session: session, request: request)
+                    })
+                    return
+                }
+                
+                // request foreground approval
+                Current.viewController?.requestUserAuthorization(session: session, request: request)
+                
             }
-            
-            // request foreground approval
-            Current.viewController?.requestUserAuthorization(session: session, request: request)
-
         }
+
     }
     
     class func notifyUser(session:Session, request:Request) {
-        
-        
-        switch UIApplication.shared.applicationState {
-            
-        case .background: // Background: then present local notification
-            guard Policy.shouldShowApprovedNotifications(for: session) else {
-                log("skip sending push notification on approved request due to policy setting")
-                return
+        dispatchMain {
+            switch UIApplication.shared.applicationState {
+                
+            case .background: // Background: then present local notification
+                guard Policy.shouldShowApprovedNotifications(for: session) else {
+                    log("skip sending push notification on approved request due to policy setting")
+                    return
+                }
+                
+                Notify.shared.presentApproved(request: request, for: session)
+                
+            case .inactive: // Inactive: wait and try again
+                dispatchAfter(delay: 1.0, task: {
+                    Policy.notifyUser(session: session, request: request)
+                })
+                
+            case .active:
+                guard Current.viewController?.presentedViewController is AutoApproveController == false
+                    else {
+                        return
+                }
+                
+                Current.viewController?.showApprovedRequest(session: session, request: request)
             }
-          
-            Notify.shared.presentApproved(request: request, for: session)
-            
-        case .inactive: // Inactive: wait and try again
-            dispatchAfter(delay: 1.0, task: {
-                Policy.notifyUser(session: session, request: request)
-            })
-            
-        case .active:
-            guard Current.viewController?.presentedViewController is AutoApproveController == false
-            else {
-                return
-            }
-            
-            Current.viewController?.showApprovedRequest(session: session, request: request)
+
         }
-        
         
     }
     
     
     class func notifyUser(errorMessage:String, session:Session) {
-        
-        switch UIApplication.shared.applicationState {
-            
-        case .background: // Background: then present local notification
-            Notify.shared.presentError(message: errorMessage, session: session)
-            
-        case .inactive: // Inactive: wait and try again
-            dispatchAfter(delay: 1.0, task: {
-                Policy.notifyUser(errorMessage: errorMessage, session: session)
-            })
-            
-        case .active:
-            guard Current.viewController?.presentedViewController is AutoApproveController == false
-                else {
-                    return
+        dispatchMain {
+            switch UIApplication.shared.applicationState {
+                
+            case .background: // Background: then present local notification
+                Notify.shared.presentError(message: errorMessage, session: session)
+                
+            case .inactive: // Inactive: wait and try again
+                dispatchAfter(delay: 1.0, task: {
+                    Policy.notifyUser(errorMessage: errorMessage, session: session)
+                })
+                
+            case .active:
+                guard Current.viewController?.presentedViewController is AutoApproveController == false
+                    else {
+                        return
+                }
+                
+                Current.viewController?.showFailedResponse(errorMessage: errorMessage, session: session)
             }
-            
-            Current.viewController?.showFailedResponse(errorMessage: errorMessage, session: session)
         }
-        
-        
     }
 }
