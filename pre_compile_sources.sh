d0fa43159a2257969efa5b945113dff9140e9f95
Alex Grinman <alex@krypt.co>
2018-06-18 10:12:11 -0400

    ignore pre compile step for xcode 10

--- pre_compile_sources.sh
+++ pre_compile_sources.sh
@@ -6,22 +6,22 @@
 #  Created by Alex Grinman on 6/13/18.
 #  Copyright © 2018 KryptCo. All rights reserved.
 
-modulesDirectory=$DERIVED_FILES_DIR/modules
-modulesMap=$modulesDirectory/module.modulemap
-modulesMapTemp=$modulesDirectory/module.modulemap.tmp
-
-mkdir -p "$modulesDirectory"
-
-cat > "$modulesMapTemp" << MAP
-module CommonCrypto [system] {
-    header "$SDKROOT/usr/include/CommonCrypto/CommonCrypto.h"
-    export *
-}
-MAP
-
-diff "$modulesMapTemp" "$modulesMap" >/dev/null 2>/dev/null
-if [[ $? != 0 ]] ; then
-mv "$modulesMapTemp" "$modulesMap"
-else
-rm "$modulesMapTemp"
-fi
+#modulesDirectory=$DERIVED_FILES_DIR/modules
+#modulesMap=$modulesDirectory/module.modulemap
+#modulesMapTemp=$modulesDirectory/module.modulemap.tmp
+#
+#mkdir -p "$modulesDirectory"
+#
+#cat > "$modulesMapTemp" << MAP
+#module CommonCrypto [system] {
+#    header "$SDKROOT/usr/include/CommonCrypto/CommonCrypto.h"
+#    export *
+#}
+#MAP
+#
+#diff "$modulesMapTemp" "$modulesMap" >/dev/null 2>/dev/null
+#if [[ $? != 0 ]] ; then
+#mv "$modulesMapTemp" "$modulesMap"
+#else
+#rm "$modulesMapTemp"
+#fi
